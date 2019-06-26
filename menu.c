6c1cd57c6f4f5f2b3840c61d3f4ea462bce6287c
Kamila Mariane <47602140+kamilamariane@users.noreply.github.com>
2019-06-25 22:02:55 -0300

    Update and rename TipoB_5x5 to menu.c

---
+++ menu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void menu(void)
+{
+	printf(
+		"\n\t\tPROGRAMA DE MANIPULAÇÃO DE MATRIZES\n\n"
+
+		"(1) Definir o tamanho da matriz A\n"
+		"(2) Definir o tamanho da matriz B\n"
+		"(3) Preencher a matriz A com valores aleatórios\n"
+		"(4) Preencher a matriz B com valores aleatórios\n"
+		"(5) Atribuir valores manualmente para os elementos da matriz A\n"
+		"(6) Atribuir valores manualmente para os elementos da matriz B\n"
+		"(7) Calcular A+B\n"
+		"(8) Calcular A-B\n"
+		"(9) Calcular A*B\n"
+		"(10) Imprimir matriz A\n"
+		"(11) Imprimir matriz B\n"
+		"(12) Imprimir matriz C\n"
+		"(13) Ler a matriz A de um arquivo\n"
+		"(14) Ler a matriz B de um arquivo\n"
+		"(15) Escrever a matriz C em um arquivo\n"
+		"(16) Sair\n");
+}
