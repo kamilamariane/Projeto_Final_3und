d4e435640dcb092d05ef79c08d0c5fa954641ae8
Kamila Mariane <47602140+kamilamariane@users.noreply.github.com>
2019-06-25 21:44:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
-a
+#include <stdio.h>
+#include <time.h> 
+#include<stdlib.h>
+
+//chamando as funcoes abaixo:
+#include "Headers/menu.h"
+#include "Headers/escolha.h"
+
+
+int main(){
+    
+  srand(time(NULL));
+  int operacao;
+  FILE*file;
+  FILE *fileout;
+
+  do{
+    menu();
+    printf("Digite sua operação: ");
+    scanf("%d", &operacao);
+    escolha(operacao);
+  }while(operacao != 16);
+  
+
+  return 0;
+
+}
